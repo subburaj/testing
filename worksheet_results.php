b1432917bb5e5fbeb059c043b5d1e6b3bd851ea4
subburaj <subburaj6587@gmail.com>
2013-08-12 19:27:19 +0530

    worksheet_results,worksheet generated file created

---
+++ worksheet_results.php
@@ -0,0 +1,146 @@
+<?php
+    $urlFormSubmit = 'ClientParent/saveworksheetresults/' . $student_id . '/' . $subject_id . '/' . $worksheet_date;
+    $urlFormCancel = 'ClientParent/worksheetresults/' . $student_id;
+
+    $attr_button_cancel = array(
+        'id'    => 'cancelWorksheetResults',
+        'class' => 'button button-small-gray button-cancel',
+        'title' => 'Cancel worksheet results'
+    );
+
+    $attr_button_submit = array(
+        'id'    => 'submitWorksheetResults',
+        'class' => 'button button-medium-green button-save',
+        'title' => 'Save worksheet results'
+    );
+
+?>
+
+<?= form_open($urlFormSubmit, array('id' => 'formWorksheetResults')) ?>
+    <fieldset class="no-margin">
+        <legend>Worksheet Results</legend>
+
+        <?= $results_table ?>
+    </fieldset>
+
+    <div class="buttons">
+        <span class="buttons">
+            <?= anchor($urlFormCancel, '<span>Cancel</span>', $attr_button_cancel) ?>
+            <?= anchor('', '<span>Save Results</span>', $attr_button_submit) ?>
+        </span>
+    </div>
+
+    <!-- Save results confirmation -->
+    <div id="dialog-confirm" title="Save results ?">
+        <p>
+            The worksheet results will be saved and you won't be allowed to reenter them again. <br/><br/>
+            Do you want to save the worksheet results for sure ?
+        </p>
+    </div>
+<?= form_close() ?>
+
+<!-- jQuery Tools -->
+<script type="text/javascript" src="<?= base_url() . 'static/js/jqueryTools/jquery.tools.min.js' ?>"></script>
+
+<script type="text/javascript">
+    $(function() {
+        // Setting up the confirmation dialog
+        $("#dialog-confirm").dialog({
+            autoOpen: false,
+            resizable: false,
+            height:220,
+            width:500,
+            modal: true,
+            buttons: {
+                'No': function() {
+                    $(this).dialog('close');
+                },
+                'Yes': function() {
+                    $('#formWorksheetResults').submit();
+                    $(this).dialog('close');
+                    return false;
+                }
+            }
+        });
+
+        // Setting up tooltips behavior for the questions & answers
+        $(".tooltip-trigger").tooltip({
+            relative: true,
+            predelay: 400,
+            delay: 0
+        });
+
+        // Setting up the right/wrong/skipped buttons' behavior
+        function setButtonOn($button) {
+            if ($button.hasClass('button-ws-right-on') || $button.hasClass('button-ws-wrong-on') || $button.hasClass('button-ws-skipped-on'))
+                return;
+
+            $table_cell = $button.closest('td');
+            $table_cell.find('input:checkbox.ws-checkbox').removeAttr('checked');
+
+            $buttonRight = $table_cell.find('a.button-ws-right-on');
+            $buttonRight.removeClass('button-ws-right-on').addClass('button-ws-right-off');
+
+            $buttonWrong = $table_cell.find('a.button-ws-wrong-on');
+            $buttonWrong.removeClass('button-ws-wrong-on').addClass('button-ws-wrong-off');
+
+            $buttonSkipped = $table_cell.find('a.button-ws-skipped-on');
+            $buttonSkipped.removeClass('button-ws-skipped-on').addClass('button-ws-skipped-off');
+
+            if ($button.hasClass('button-ws-right-off')) {
+                $button.removeClass('button-ws-right-off').addClass('button-ws-right-on');
+                $table_cell.find('input:checkbox.ws-checkbox-right').attr('checked', 'checked');
+            }
+
+            if ($button.hasClass('button-ws-wrong-off')) {
+                $button.removeClass('button-ws-wrong-off').addClass('button-ws-wrong-on');
+                $table_cell.find('input:checkbox.ws-checkbox-wrong').attr('checked', 'checked');
+            }
+
+            if ($button.hasClass('button-ws-skipped-off')) {
+                $button.removeClass('button-ws-skipped-off').addClass('button-ws-skipped-on');
+                $table_cell.find('input:checkbox.ws-checkbox-skipped').attr('checked', 'checked');
+            }
+        }
+
+        // Handling the tri-state right/wrong/skipped type button's behavior
+        $('.button-ws').click(function() {
+            setButtonOn($(this));
+
+            return false;
+        });
+
+        // Show confirmation dialog when trying to save the results
+        $('#submitWorksheetResults').live('click', function() {
+            $('#dialog-confirm').dialog('open');
+            return false;
+        });
+
+        // Handle form submittal
+        $('#formWorksheetResults').submit(function() {
+            var $form = $(this);
+
+            $.ajax({
+               url: $form.attr('action'),
+               data: $form.serialize(),
+               dataType: 'json',
+               type: 'post',
+               success: function(ajaxData) {
+                   if (ajaxData.success == 'yes') {
+                       $('#content-wrapper').html(ajaxData.results);
+
+                       humanMsg.displayMsg(ajaxData.message);
+
+                       // Redirecting to the worksheets scheduling page of the student
+                       // $(window.location).attr('href', $SITE_URL + '/<?= $urlFormCancel ?>');
+                   }
+                   else {
+                       humanMsg.displayMsg(errorMessage(ajaxData.message));
+                   }
+               }
+            });
+
+            return false;
+        });
+    });
+</script>
---
+++ worksheets_generate.php
@@ -0,0 +1,29 @@
+
+<div id="centerMain">
+            <?= form_open('ClientParent/generateworksheets', array('id' => 'formGenerateWorksheets')) ?>
+                <fieldset class="icon-presentation-categories no-margin">
+                    <legend>Manage Worksheets</legend>
+                    
+                    <div class="formRow">
+                        <label for="inputPresentationCategory">Student Name</label>
+                        <?= $this->form_field_generator->getInputFormField('inputPresentationCategory', $value = 'Should be one of the registered children') ?>
+                    </div>
+
+                    <div class="formRow">
+                        <label for="textareaPresentationCategoryDescription">Subject</label>
+                        <?= $this->form_field_generator->getInputFormField('inputPresentationCategory', $value = 'Choose a subject') ?>
+                    </div>
+
+                    <div class="formRow">
+                        <label for="textareaPresentationCategoryOptions">Number of weeks</label>
+                        <?= $this->form_field_generator->getInputFormField('inputPresentationCategory', $value = 'Can generate worksheets for few weeks, if you are travelling') ?>
+                    </div>
+                </fieldset>
+
+                <div class="buttons">
+                    <span>
+                        <button type="submit" name="submit" id="submitGenerate" class="button-small-green button-add"><span>Generate</span></button>
+                    </span>
+                </div>
+            <?= form_close() ?>
+</div>
